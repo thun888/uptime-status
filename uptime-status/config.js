d87ef99fa7476793226c8ef7003d66a9203025f9
thun888 <63234268+thun888@users.noreply.github.com>
2023-04-22 15:20:11 +0800

    Update config.js

--- uptime-status/config.js
+++ uptime-status/config.js
@@ -1,36 +1,33 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'Server Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1070350-c0ef108c3d63eed50411263f',
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 14,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '(MC)服务平台',
+      url: 'https://mcweb.hzchu.top/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/thun888'
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://blog.hzchu.top/'
     },
   ],
 };
